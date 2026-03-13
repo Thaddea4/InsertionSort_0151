e22c9b38091107e2fa7f6822c41a4c82bde5f165
Thaddea4 <adeyasyie@gmail.com>
2026-03-13 14:17:30 +0700

    Menambahkan input jumlah data array

--- Insertion_Sort.cpp
+++ Insertion_Sort.cpp
@@ -5,5 +5,8 @@ int arr[20];       //membuat array dengan panjang data 20
 int n;             //membuat variable inputan n
 
 void input (){ //procedure input
-
+while (true){
+    cout << "Masukkan Jumlah Data Pada Array : ";
+    cin >> n;
+}
 }
