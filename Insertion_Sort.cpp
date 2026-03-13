5c6ed71b81a9901728466d5025611c4d1ae36099
Thaddea4 <adeyasyie@gmail.com>
2026-03-13 14:41:09 +0700

    Menambahan prosedur insertion sort

--- Insertion_Sort.cpp
+++ Insertion_Sort.cpp
@@ -21,7 +21,11 @@ cout << "=============================================";
 cout << "============Masukkan Element Array===========";
 cout << "=============================================";
 
-
+for (int i = 0; i < n; i++ ){
+    cout << "Data ke-" << (i+1) << ": ";
     cin >> arr[i];
+
+
+void insertionsort(){ //procedure insertion sort
 
 }
